5e3f75151bf3527b81adc4c50c20781e92bc1cfb
Shanthosh kumar <cb.en.u4aee14046@cb.students.amrita.edu>
2017-12-03 03:11:04 +0530

    added favicon

--- app/src/js/sections/citySection.js
+++ app/src/js/sections/citySection.js
@@ -2,12 +2,22 @@
 'use strict';
 
 var Section = require('../classes/SectionClass');
-
 var TextPanel = require('../objects3D/TextPanelObject3D');
-
-
+var Smoke = require('../objects3D/SmokeObject3D');
 var citySection = new Section('city');
 
+var smoke = new Smoke({
+  planesNumber: 3,
+  frontColor: '#4c4c4c',
+  backColor: '#ffffff',
+  data: [
+    { positionX : 0, positionY: 0, positionZ: 0, rotationZ: 2.7, scale: 8.5 },
+    { positionX : 0, positionY: 0, positionZ: 0, rotationZ: 1.4, scale: 5.8 },
+    { positionX : 0, positionY: 0, positionZ: 0, rotationZ: 1.6, scale: 7.4 }
+  ]
+});
+citySection.add(smoke.el);
+smoke.el.visible = false;
 
 var techTitle =  new TextPanel(
   'TECHNICAL DEPARTMENTS',
@@ -18,7 +28,7 @@ var techTitle =  new TextPanel(
     lineSpacing: 40
   }
 );
-techTitle.el.position.set(0,15,0);
+techTitle.el.position.set(0,15,5);
 citySection.add(techTitle.el);
 techTitle.in();
 
@@ -31,7 +41,7 @@ var cse = new TextPanel(
     lineSpacing: 40
   }
 );
-cse.el.position.set(-15,-5,0);
+cse.el.position.set(-15,-5,5);
 citySection.add(cse.el);
 cse.in();
 
@@ -64,51 +74,31 @@ citySection.onStop(function (way) {
 
 });
 
+var smokePlaying = false;
+
+citySection.smokeStart = function () {
+  if (smokePlaying) {
+    return false;
+  }
+
+  smokePlaying = true;
+
+  smoke.start();
+
+  smoke.el.visible = true;
+};
+
+citySection.smokeStop = function () {
+  if (!smokePlaying) {
+    return false;
+  }
+
+  smokePlaying = false;
+
+  smoke.stop();
+
+  smoke.el.visible = false;
+};
+
+
 module.exports = citySection;
-
-
-
-/*'use strict';
-
-var Section = require('../classes/SectionClass');
-
-var TextPanel = require('../objects3D/TextPanelObject3D');
-
-var citySection = new Section('city');
-
-var cse = new TextPanel(
-  'C S E\n E C E\n E E E\n M E C H\n C H E M\n A E R O\n C I V I L',
-  {
-    align: 'left',
-    style: '',
-    size: 35,
-    lineSpacing: 40
-  }
-);
-cse.el.position.set(20,-20,0);
-citySection.add(cse.el);
-
-var cs = new TextPanel(
-  'A S C O M\n C E N\n C Y B E R    S E C U R I T Y\n M S W\n M A T H S\n S C I E N C E\n E N G L I S H',
-  {
-    align: 'left',
-    style: '',
-    size: 35,
-    lineSpacing: 40
-  }
-);
-cs.el.position.set(20,20,0);
-citySection.add(cs.el);
-
-citySection.onIn(function () {
-  cse.in();
-  cs.in();
-});
-
-citySection.onOut(function (way) {
-  cse.out(way);
-  cs.out(way);
-
-});
-
-module.exports = citySection;*/
